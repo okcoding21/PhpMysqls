636bacf5983ec351ca501adb8263bc484870dbbd
OkCoding <31806735+okcoding21@users.noreply.github.com>
2020-08-23 12:59:05 +0530

    Create DatabaseConfig.php

---
+++ DatabaseConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+//Define your host here.
+$HostName = "fdb19.biz.nf";
+
+//Define your database username here.
+$HostUser = "3423862_tulsipaints";
+
+//Define your database password here.
+$HostPass = "Killer@007";
+
+//Define your database name here.
+$DatabaseName = "3423862_tulsipaints";
+
+?>
